9a19eb18d665721451b7fbb7baa3963abfd5160b
MikkaHYTBG <44730779+MikkaHYT@users.noreply.github.com>
2022-12-19 20:00:20 +0000

    Initial Commit

---
+++ main.sh
@@ -0,0 +1,5 @@
+echo "if this doesn't work, just download the reply as a zip and run it manually since repl has to be ran manually thanks to the nix update"
+cd 1449/Linux
+chmod +X TerrariaServer.bin.x86_64
+chmod +X TerrariaServer
+bash TerrariaServer http:///
